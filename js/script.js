7bc3db8a74a5e26776b4fe1afb9ad14d50a2aa49
emcahell <emmanuelltowers@gmail.com>
2024-11-12 22:57:24 -0400

    añadi boton de cerrar mensaje

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 const cora = document.getElementById('cora');
 const boxMessage = document.getElementById('box-message');
 const message = document.getElementById('message');
+const btnClose = document.getElementById('btn-close');
 
 cora.addEventListener('click', () =>{
     message.textContent = 'Te Amu <3';
     boxMessage.classList.add('active');
+    btnClose.classList.add('active');
+})
+
+btnClose.addEventListener('click', () =>{
+    boxMessage.classList.remove('active');
+    btnClose.classList.remove('active');
 })
